b65cfee770e872becef276e0bc3a3cd9749d61b4
Endri Xhukellari <endrixh7@yahoo.com>
2024-01-31 21:06:28 +0100

    React Query Basics Done ✅

--- starter/src/Items.jsx
+++ starter/src/Items.jsx
@@ -1,8 +1,40 @@
 import SingleItem from './SingleItem';
-const Items = ({ items }) => {
+import {useQuery} from '@tanstack/react-query';
+import customFetch from './utils';
+
+const Items = () => {
+  // Destructure 'isLoading' and 'data'
+  const {isLoading, data, error, isError} = useQuery(
+    {
+      queryKey:['tasks'],
+      queryFn: async () => {
+        // Destructure data here and return data
+        const {data} = await customFetch.get('/');
+        return data;
+      },
+    },
+  )
+
+  if (isLoading) {
+    return <p style={{margin:'1rem'}}> ...Loading Data</p>;
+  }
+
+  // Front end error
+  // if (isError) {
+  //   return <p style={{margin:'1rem'}}> ...Error</p>;
+  // }
+
+  // Server Error
+  if (error) {
+    return <p style={{margin:'1rem'}}> {error.response.data}</p>;
+  }
+
+
+
+
   return (
     <div className='items'>
-      {items.map((item) => {
+      {data.taskList.map((item) => {
         return <SingleItem key={item.id} item={item} />;
       })}
     </div>
--- starter/src/SingleItem.jsx
+++ starter/src/SingleItem.jsx
@@ -1,10 +1,40 @@
+import customFetch from './utils';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+// This will take singel param and will be used below, item param is used for id
 const SingleItem = ({ item }) => {
+  // Refetch data using queryClient
+  const queryClient = useQueryClient();
+  // Create function and add the alias for that function
+  const {mutate: editTask} = useMutation({
+    mutationFn:({taskId, isDone}) => {
+      return customFetch.patch(`/${taskId}`, {isDone});
+    },
+    // Add onSuccess
+    onSuccess: () => {
+      queryClient.invalidateQueries({queryKey:['tasks']});
+    },
+  });
+
+  // Delete Task
+  const {mutate: deleteTask, isLoading} = useMutation({
+    mutationFn:(taskId) => {
+      return customFetch.delete(`/${taskId}`);  
+    },
+    // Add onSuccess
+    onSuccess: () => {
+      queryClient.invalidateQueries({queryKey:['tasks']});
+    },
+  });
+
   return (
     <div className='single-item'>
       <input
         type='checkbox'
         checked={item.isDone}
-        onChange={() => console.log('edit task')}
+        // Invoke the 'mutate' function here, alias editTask, make sure to add dynamic url
+        // This line will change from true to false : isDone:!item.isDone
+        onChange={() => editTask({taskId:item.id, isDone:!item.isDone})}
       />
       <p
         style={{
@@ -17,7 +47,8 @@ const SingleItem = ({ item }) => {
       <button
         className='btn remove-btn'
         type='button'
-        onClick={() => console.log('delete task')}
+        disabled={isLoading}
+        onClick={() => deleteTask(item.id)}
       >
         delete
       </button>
